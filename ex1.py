2f2d0e49e934f25723e0e1e5c473df397a96401a
vinothini92 <vinothinicbe@gmail.com>
2013-06-14 10:09:53 +0530

    first commit

---
+++ ex1.py
@@ -0,0 +1,15 @@
+#program to find largest of three numbers
+print "enter first number "
+x = raw_input()
+print "enter second number"
+y = raw_input()
+print "enter third number"
+z = raw_input()
+if (x > y and x > z) :
+ print " x is greater"
+elif (y > x and y > z) :
+ print "y is greater "
+else : 
+ print "z is greater"
+
+
